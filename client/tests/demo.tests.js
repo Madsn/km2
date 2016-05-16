bc9252b90c5d16998b2ab5a7ab955db95c465cc9
Madsn <m@madsn.net>
2016-05-16 16:14:49 +0200

    Testing CI (should fail)

--- client/tests/demo.tests.js
+++ client/tests/demo.tests.js
@@ -4,6 +4,6 @@ describe('my module', function () {
   it('does something that should be tested', function () {
     // This code will be executed by the test driver when the app is started
     // in the correct mode
-    chai.assert.equal(true, true);
+    chai.assert.equal(true, false);
   })
 })
