5d13a1372ba5df07e777c337fb94e6b7461e624d
madsn <m@madsn.net>
2016-05-31 19:13:55 +0000

    Dashboard data from JS array

--- client/modules/kennel-dashboard/kennel-dashboard.js
+++ client/modules/kennel-dashboard/kennel-dashboard.js
@@ -1,3 +1,20 @@
+Template.occupants.helpers({
+  bookings() {
+    return [
+        {_id: 1, type: "Boarding", customer: "Thisen", details: "1 cat for 10 days", status: "2 days till checkout" },
+        {_id: 2, type: "Boarding", customer: "Madsn", details: "1 dog for 30 days", status: "2 days till checkout" }
+      ];
+  }
+});
+
+Template.due_to_check_in.helpers({
+  bookings() {
+    return [
+        {_id: 3, type: "Boarding", customer: "Thisen", details: "1 cat for 2 days", status: "Checking in today" },
+        {_id: 4, type: "Boarding", customer: "Madsn", details: "1 dog for 3 days", status: "Checking in today" }
+      ];
+  }
+})
 
 FlowRouter.route('/kenneldash', {
   name: 'Kenneldash',
